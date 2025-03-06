ff55f10e58cdbda052c4eacab41787da66777fdf
SewonKim0 <sewonkim0black@gmail.com>
2025-03-05 22:44:28 -0500

    Feed Page: added color palette, feed markdown, sample feed data

--- front-end/src/Feed.js
+++ front-end/src/Feed.js
@@ -1,11 +1,74 @@
 import './Feed.css'
 
 const Feed = () => {
+    const SAMPLE_DATA = [
+        {
+            title: "Book Title",
+            year: "Book Year",
+            author: "Book Author",
+            bookUrl: ".",
+            imgUrl: "."
+        },
+        {
+            title: "Book Title",
+            year: "Book Year",
+            author: "Book Author",
+            bookUrl: ".",
+            imgUrl: "."
+        },
+        {
+            title: "Book Title",
+            year: "Book Year",
+            author: "Book Author",
+            bookUrl: ".",
+            imgUrl: "."
+        },
+        {
+            title: "Book Title",
+            year: "Book Year",
+            author: "Book Author",
+            bookUrl: ".",
+            imgUrl: "."
+        },
+        {
+            title: "Book Title",
+            year: "Book Year",
+            author: "Book Author",
+            bookUrl: ".",
+            imgUrl: "."
+        },
+        {
+            title: "Book Title",
+            year: "Book Year",
+            author: "Book Author",
+            bookUrl: ".",
+            imgUrl: "."
+        }
+    ]
+
     return (
         <main className="Feed">
             <h1>
-                Feed
+                For You
             </h1>
+
+            {/* Books */}
+            <div>
+                {SAMPLE_DATA.map((book, index) => (
+                    <div key={index} className="Book">
+                        <img src={book.imgUrl} alt="Book Cover" />
+                        <h2>{book.title}</h2>
+                        <p>{book.year}</p>
+                        <p>{book.author}</p>
+                        
+                        <a href={book.bookUrl}>
+                            <button>
+                                Show Interest
+                            </button>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </main>
     )
 }
