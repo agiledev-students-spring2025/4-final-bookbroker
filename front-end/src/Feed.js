3d5b6e5fe770591b1dc9f49c78312172a922936e
SewonKim0 <sewonkim0black@gmail.com>
2025-03-06 17:28:43 -0500

    Feed Page: feed page design complete

--- front-end/src/Feed.js
+++ front-end/src/Feed.js
@@ -48,20 +48,23 @@ const Feed = () => {
 
     return (
         <main className="Feed">
-            <h1>
+            <h1 className="feed-title">
                 For You
             </h1>
 
             {/* Books */}
-            <div>
+            <div className="feed-books">
                 {SAMPLE_DATA.map((book, index) => (
-                    <div key={index} className="Book">
+                    <div key={index} className="feed-book">
                         <img src={book.imgUrl} alt="Book Cover" />
-                        <h2>{book.title}</h2>
-                        <p>{book.year}</p>
-                        <p>{book.author}</p>
                         
-                        <a href={book.bookUrl}>
+                        <div className="feed-book-text">
+                            <h2>{book.title}</h2>
+                            <p>{book.year}</p>
+                            <p>{book.author}</p>
+                        </div>
+
+                        <a href={book.bookUrl} className="feed-book-button">
                             <button>
                                 Show Interest
                             </button>
