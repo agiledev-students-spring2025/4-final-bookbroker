5fd171f3e0409aa32efdff220809264a2a9aaab4
StephenS2021 <sspencerwong28@gmail.com>
2025-03-06 16:01:43 -0500

    filling out profile page

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -1,11 +1,26 @@
+
 import './Profile.css'
 
 const Profile = () => {
+    const SAMPLE_PROFILE = {
+        username: 'username',
+        email: 'email',
+        location: 'location',
+        ratings: 'ratings',
+        profileUrl: '.',
+
+    }
+
     return (
         <main className="Profile">
-            <h1>
-                Profile
-            </h1>
+            <h1>Profile</h1>
+            <div>
+                <img src={SAMPLE_PROFILE.profileUrl} alt="Profile Img" class="w-120"/>
+                <p class="text-lg">{SAMPLE_PROFILE.username}</p>
+                <p>{SAMPLE_PROFILE.email}</p>
+                <p>{SAMPLE_PROFILE.location}</p>
+                <p>{SAMPLE_PROFILE.ratings}</p>
+            </div>
         </main>
     )
 }
