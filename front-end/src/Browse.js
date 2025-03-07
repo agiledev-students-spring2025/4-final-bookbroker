5bbdf50c5d4514688259450fd5639027c61240eb
Rainn-J <rj2348@nyu.edu>
2025-03-07 12:01:15 -0500

    browse/category/genre pages

--- front-end/src/Browse.js
+++ front-end/src/Browse.js
@@ -1,11 +1,36 @@
 import './Browse.css'
+import { useNavigate } from "react-router-dom";
 
 const Browse = () => {
+    const navigate = useNavigate();
+
+    const routes = {
+        "Popular Now": "/browse/popular",
+        "Newly Added": "/browse/newly-added",
+        "By Category": "/browse/by-category",
+        "Search": "/browse/search",
+      };
+
     return (
         <main className="Browse">
-            <h1>
-                Browse
-            </h1>
+            <div className="titlebox">
+                <h1 className="title">Browse</h1>
+            </div>
+            
+
+            <div className="category-list">
+                {["Popular Now", "Newly Added", "By Category", "Search"].map((item, index) => (
+                    <button key={index} onClick={() => navigate(routes[item])} className="category-btn">
+                    <span className="underline">{item}</span>
+                    </button>
+                ))}
+            </div>
+
+            <div className="bottom-nav">
+                {["Home", "Browse", "Feed", "Messages", "Profile"].map((tab, index) => (
+                <button key={index} onClick={() => navigate(`/${tab.toLowerCase()}`)}  className="nav-item">{tab}</button>
+                ))}
+            </div>
         </main>
     )
 }
