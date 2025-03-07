3d615fbf15b93b1ea357ed55965dc9a17c59830d
StephenS2021 <sspencerwong28@gmail.com>
2025-03-07 13:19:19 -0500

    Added profile styling

--- front-end/src/Profile.js
+++ front-end/src/Profile.js
@@ -37,9 +37,9 @@ const Profile = () => {
     return (
         <main className="Profile">
             <h1 class="text-[30px] font-medium">Profile</h1>
-            <div class="flex flex-row">
+            <div class="flex flex-row pl-4">
                 <img  src={SAMPLE_PROFILE.profileUrl} alt="Profile Img" class="w-120 bg-gray-500"/>
-                <div class="p-4">
+                <div class="pl-4">
                     <p class="text-lg">{SAMPLE_PROFILE.username}</p>
                     <p>{SAMPLE_PROFILE.email}</p>
                     <p>{SAMPLE_PROFILE.location}</p>
@@ -51,17 +51,19 @@ const Profile = () => {
                 <h1 class="text-[24px] font-medium mb-4 mt-4 pl-4">Wishlist</h1>
                 <div class="flex flex-col space-y-4 max-sm:items-center">
                 {SAMPLE_DATA.map((book, index) => (
-                    <div key={index} className="Book" class="border-2 border-solid p-4">
-                        <img src={book.imgUrl} alt="Book Cover" />
-                        <h2>{book.title}</h2>
-                        <p>{book.year}</p>
-                        <p>{book.author}</p>
-                        
-                        <a href={book.bookUrl}>
-                            <button>
-                                Edit
-                            </button>
-                        </a>
+                    <div key={index} className="Book" class="border-2 border-solid p-4 flex flex-row">
+                        <img src={book.imgUrl} alt="Book Cover" class="w-120 bg-gray-500" />
+                        <div class="pl-4">
+                            <h2>{book.title}</h2>
+                            <p>{book.year}</p>
+                            <p>{book.author}</p>
+                            
+                            <a href={book.bookUrl}>
+                                <button>
+                                    Edit
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 ))}
                 </div>
@@ -71,17 +73,19 @@ const Profile = () => {
                 <h1 class="text-[24px] font-medium mb-4 mt-4 pl-4">Your Listings</h1>
                 <div class="flex flex-col space-y-4 max-sm:items-center">
                 {SAMPLE_DATA.map((book, index) => (
-                    <div key={index} className="Book" class="border-2 border-solid p-4">
-                        <img src={book.imgUrl} alt="Book Cover" />
-                        <h2>{book.title}</h2>
-                        <p>{book.year}</p>
-                        <p>{book.author}</p>
-                        
-                        <a href={book.bookUrl}>
-                            <button>
-                                Edit
-                            </button>
-                        </a>
+                    <div key={index} className="Book" class="border-2 border-solid p-4 flex flex-row">
+                        <img src={book.imgUrl} alt="Book Cover" class="w-120 bg-gray-500"/>
+                        <div class="pl-4">
+                            <h2>{book.title}</h2>
+                            <p>{book.year}</p>
+                            <p>{book.author}</p>
+                            
+                            <a href={book.bookUrl}>
+                                <button>
+                                    Edit
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 ))}
                 </div>
