4f7da515e2ac965af2d80a7820d47bfa9de6d9e9
isaac1000000 <isaac.fisher@nyu.edu>
2025-03-09 16:09:15 -0400

    Tailwind config, book pages

--- front-end/src/BookPage.js
+++ front-end/src/BookPage.js
@@ -14,14 +14,20 @@ const BookPage = () => {
             .catch(err => console.error(err))
     }, [book, id])
 
+    const bookImageSource = process.env.REACT_APP_MOCK_IMG_SRC
+
     return (
-        <main className="BookPage">
-            <h1 className="text-4xl">{book.title}</h1>
-            <h2>Offered by: {book.user}</h2>
-            <img src="https://picsum.photos/200/300" alt="book" />
-            <h3>{book.author}, {book.year}</h3>
-            <p>{book.desc}</p>
-            <p>{book.isbn}, {book.genre}</p>
+        <main className="BookPage content-center mt-10">
+            <div className="header text-center mb-5">            
+                <h1 className="text-4xl">{book.title ? book.title : "[NO TITLE]"}</h1>
+                <h2 className="text-2xl">Offered by: {book.user ? book.user : "[NO USER]"}</h2>
+            </div>
+            <img src={bookImageSource} alt="book" className="mx-auto mb-5" />
+            <div className="book-description text-left mx-10">            
+                <h3>{book.author ? book.author: "[NO AUTHOR]"}, {book.year ? book.year : "[NO DATE]"}</h3>
+                <p>{book.desc ? book.desc : "[NO DESC]"}</p>
+                <p>ISBN: {book.isbn ? book.isbn : "[NO ISBN]"}, Genre: {book.genre ? book.genre : "[NO GENRE]"}</p>
+            </div>
         </main>
     )
 }
