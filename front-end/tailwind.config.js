8be0b6343a48909bad9e0e79203a112a4c5e9332
StephenS2021 <sspencerwong28@gmail.com>
2025-03-09 16:45:51 -0400

    Added tailwind custom colors and font sizes for standardization

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: '#38261e',
+        orange: '#ff9700',
+        background: '#eadbd6'
+      },
+      fontSize: {
+        'heading-lg': '2.0rem',
+        'heading-md': '1.5rem', 
+        'heading-sm': '1.25rem', 
+      },
+    },
   },
   plugins: [],
 }
