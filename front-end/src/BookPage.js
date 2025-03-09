b9ac1bf2a57d0d9299cbce10aa9f68ed8fdb5559
isaac1000000 <isaac.fisher@nyu.edu>
2025-03-09 15:38:55 -0400

    Fetches book info

--- front-end/src/BookPage.js
+++ front-end/src/BookPage.js
@@ -1,13 +1,27 @@
 import './BookPage.css'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const BookPage = () => {
     const { id } = useParams()
+    const [book, setBook] = useState({})
+
+    useEffect(() => {
+        axios
+            .get(`https://my.api.mockaroo.com/book?id=${id}&key=${process.env.REACT_APP_MOCK_BOOK_API_KEY}`)
+            .then(response => setBook(response.data))
+            .catch(err => console.error(err))
+    }, [book, id])
+
     return (
         <main className="BookPage">
-            <h1>
-                Book with id: { id }
-            </h1>
+            <h1 className="text-4xl">{book.title}</h1>
+            <h2>Offered by: {book.user}</h2>
+            <img src="https://picsum.photos/200/300" alt="book" />
+            <h3>{book.author}, {book.year}</h3>
+            <p>{book.desc}</p>
+            <p>{book.isbn}, {book.genre}</p>
         </main>
     )
 }
