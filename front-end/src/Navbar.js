f0253e08bac7420b67e7779d2a8ac9482ab64ac4
shayne773 <shayne773@gmail.com>
2025-03-10 19:57:59 -0400

    removed text for navbar icons

--- front-end/src/Navbar.js
+++ front-end/src/Navbar.js
@@ -7,23 +7,18 @@ const Navbar = () => {
     <nav className="navbar">
       <NavLink to="/home" className="nav-item">
         <FaHome className="nav-icon" />
-        <span>Home</span>
       </NavLink>
       <NavLink to="/browse" className="nav-item">
         <FaSearch className="nav-icon" />
-        <span>Browse</span>
       </NavLink>
       <NavLink to="/feed" className="nav-item">
         <FaBook className="nav-icon" />
-        <span>Feed</span>
       </NavLink>
       <NavLink to="/messages" className="nav-item">
         <FaEnvelope className="nav-icon" />
-        <span>Messages</span>
       </NavLink>
       <NavLink to="/profile" className="nav-item">
         <FaUser className="nav-icon" />
-        <span>Profile</span>
       </NavLink>
     </nav>
   );
