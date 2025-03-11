1e1275b96c54eca8bde4552a41ec276b464d2513
shayne773 <shayne773@gmail.com>
2025-03-10 20:05:43 -0400

    fixed formatting

--- front-end/src/Navbar.js
+++ front-end/src/Navbar.js
@@ -1,24 +1,23 @@
 import { NavLink } from 'react-router-dom';
-import './Navbar.css';
 import { FaHome, FaSearch, FaEnvelope, FaUser, FaBook } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="navbar fixed -bottom-0 w-full h-12 flex justify-evenly content-center bg-brown">
-      <NavLink to="/home" className="nav-item">
-        <FaHome className="nav-icon" />
+    <nav className="fixed bottom-0 w-full h-12 flex bg-brown">
+      <NavLink to="/home" className="flex-1 flex justify-center items-center py-2">
+        <FaHome className="text-white text-2xl" />
       </NavLink>
-      <NavLink to="/browse" className="nav-item">
-        <FaSearch className="nav-icon" />
+      <NavLink to="/browse" className="flex-1 flex justify-center items-center py-2">
+        <FaSearch className="text-white text-2xl" />
       </NavLink>
-      <NavLink to="/feed" className="nav-item">
-        <FaBook className="nav-icon" />
+      <NavLink to="/feed" className="flex-1 flex justify-center items-center py-2">
+        <FaBook className="text-white text-2xl" />
       </NavLink>
-      <NavLink to="/messages" className="nav-item">
-        <FaEnvelope className="nav-icon" />
+      <NavLink to="/messages" className="flex-1 flex justify-center items-center py-2">
+        <FaEnvelope className="text-white text-2xl" />
       </NavLink>
-      <NavLink to="/profile" className="nav-item">
-        <FaUser className="nav-icon" />
+      <NavLink to="/profile" className="flex-1 flex justify-center items-center py-2">
+        <FaUser className="text-white text-2xl" />
       </NavLink>
     </nav>
   );
