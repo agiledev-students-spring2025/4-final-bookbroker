dfa1faa1b060e2fd6640a546f40b2ef72b0120ab
isaac1000000 <isaac.fisher@nyu.edu>
2025-04-20 22:25:04 -0400

    small changes, stashing for next

--- front-end/src/Messages.js
+++ front-end/src/Messages.js
@@ -26,15 +26,15 @@ const Messages = () => {
 
     return (
       <main>
-        <h2 class='font-bold text-center p-4'>Messages</h2>
+        <h2 className='font-bold text-center p-4'>Messages</h2>
 
-        <ul class='space-y-4'>
+        <ul className='space-y-4'>
         {messages.map((message, index) => (
-            <li key={index} class='border-solid border-2 border-black p-2 '>
+            <li key={index} className='border-solid border-2 border-black p-2 '>
                 {/* Clickable dynamic link to new page */}
                 <Link to={`/messages/${message.otherUser}`}>
                     {/* Div around all content used to allign time stamp to the right */}
-                    <div class='flex w-full justify-between'>
+                    <div className='flex w-full justify-between'>
 
                         {/* Div box for pfp, username, and text*/}
                         <div className="flex items-center space-x-3">
@@ -49,7 +49,7 @@ const Messages = () => {
                         </div>
 
                         {/* Include timestamp of message*/}
-                        <p class='text-sm text-orange absolute right-2'> | {new Date(message.timestamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
+                        <p className='text-sm text-orange absolute right-2'> | {new Date(message.timestamp).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</p>
                     </div>
                 </Link>
             </li>
