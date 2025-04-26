bd4b296b81a66ec732578bb6ca1cefb856e0cf0b
shayne773 <shayne773@gmail.com>
2025-04-26 17:44:25 -0400

    added feature to delete wishlist/offered books

--- front-end/src/Profile/MyBooks.js
+++ front-end/src/Profile/MyBooks.js
@@ -1,6 +1,6 @@
 import './MyBooks.css';
 import { useEffect, useState } from 'react';
-import { FaBookOpen, FaAngleLeft} from 'react-icons/fa';
+import { FaBookOpen, FaAngleLeft, FaTrash } from 'react-icons/fa';
 import { generateBooks } from '../MockData';
 import { Link } from 'react-router-dom';
 
@@ -25,6 +25,28 @@ const MyBooks = () => {
     })
   }, []);
 
+  const handleDelete = (bookId) => {
+    console.log(bookId)
+    const token = localStorage.getItem("token");
+    fetch(`${process.env.REACT_APP_SERVER_ADDRESS}/user/wishlist/${bookId}`, {
+        method: "DELETE",
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    })
+    .then(res => {
+        if(res.ok){
+            setWishlistBooks(prevBooks => prevBooks.filter(book => book._id !== bookId))
+        }
+        else{
+            console.error("Failed to delete book from wishlist");
+        }
+    })
+    .catch(err => {
+        console.error("Error deleting book:", err);
+    });
+  }
+
   return (
     <div>
         <div className="titlebox wishlistHeader">
@@ -41,7 +63,15 @@ const MyBooks = () => {
                 <li key={index} className="wishlistItem">
                     <FaBookOpen className="bookIcon" />
                     <strong>{book.title}</strong>
+                    <button 
+                      className="deleteButton"
+                      onClick={() => handleDelete(book._id)} 
+                      style={{ marginLeft: "10px", background: "none", border: "none", cursor: "pointer" }}
+                    >
+                      <FaTrash />
+                    </button>
                 </li>
+                
                 ))
             ) : (
                 <li>Loading wishlist...</li>
